aada5f66421d8e63afa1996b1efdb12b1a66dbe4
Campbell <campbellzc@s.dcsdk12.org>
2013-09-24 07:36:45 -0600

    finished standard deviation

--- Stats.java
+++ Stats.java
@@ -1,13 +1,15 @@
 public class Stats {
 	public static void main(String[] args) {
-		int[] a = {1, 2, 3, 4, 5, 6, 7, 8};
+		int[] a = {1, 2, 3, 4, 5, 6, 7};
 
 		//max(a);
 		//min(a);
 		//mean(a);
 		//print(median(a));
 		//print(quartileOne(a));
-		//print(quartileThree(a));
+		//print(quartileThree(a)); Not Finished!!
+		//mode(a);
+		standardDeviation(a);
 	}
 	public static void print(double a) {
 		System.out.println(a);
@@ -50,15 +52,15 @@ public class Stats {
 		int length = 0;
 		for (int i = 0; i<a.length; i++) {
 			length++;
-			
-			
+
+
 		}
 		if (length % 2 == 0) {
 			median = (length /2.0) + 0.5;
 		} else {
 			median = a[length / 2];
 		}
-		
+
 		return median;
 	}
 	public static double quartileOne(int[] a) {
@@ -72,7 +74,7 @@ public class Stats {
 		} else {
 			median = a[(length - 1) / 4];
 		}
-		
+
 		return median;
 	}
 	public static double quartileThree(int[] a) {
@@ -84,25 +86,51 @@ public class Stats {
 		if (length % 2 == 0) {
 			median = (((double)a[length-1] / 4) * 3) + 0.5;
 		} else {
-			median = (((double)a[length-1] / 4) * 3) + 0.25;
+			median = (double)a[((a.length - 1) / 4) * 3];
 		}
-		
+
 		return median;
 	}
 	public static int mode(int[] a) {
-		int most = (a.length-1);
-		int occurance = 0;
-		int num = 0;
-		for (int i = 0; i<a.length-1; i++) {
-			if (a[i] == a[most]) {
-				occurance++;
-				most--;
-				num = i;
+		int length = 0;
+		for (int j=0; j<a.length; j++) {
+			length++;
+		}
 
-			} else {
-				most--;
+		int num = a[0];
+
+		int finalCount = 0;
+
+		for (int i=0; i<a.length; i++) {
+			int occurance = 0;
+			for (int r=0; r<a.length; r++) {
+
+				if (a[r] == a[i]) {
+
+					occurance++;
+				}
+			}
+			if (occurance > finalCount) {
+
+				finalCount = occurance;
+				num = a[i];
 			}
 		}
-		
+		System.out.println("The mode is: " + num);
+		System.out.println("It occurs " + finalCount + " time(s).");
+		return num;
+	}
+	public static double standardDeviation(int[] a) {
+		double total = 0.0;
+		double sum = 0.0;
+		double mean = mean(a);
+
+		for (int i = 0; i<a.length; i++) {
+			sum += Math.pow((mean - a[i]), 2);
+		}
+		sum = sum / (a.length-1);
+		total = Math.sqrt(sum);
+		System.out.println("The standard deviation of this array is: " + total);
+		return total;
 	}
 }
